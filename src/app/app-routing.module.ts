346c76aabfb11419fce9bac558b7fa6e7757b671
Jhonantan Ernane <jhonsoad@live.com>
2019-04-07 21:22:06 -0300

    navegação para landing ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'landing', loadChildren: './landing/landing.module#LandingPageModule' },
-  { path: 'login', loadChildren: './landing/login/login.module#LoginPageModule' },
+  {
+    path: '',
+    redirectTo: 'landing',
+    pathMatch: 'full'
+  },
+  {
+    path: 'landing',
+    loadChildren: './landing/landing.module#LandingPageModule'
+  },
+  {
+    path: 'login',
+    loadChildren: './landing/login/login.module#LoginPageModule'
+  },
 ];
 
 @NgModule({
