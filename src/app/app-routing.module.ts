e8045dbe5bcd3dbfb4a5d10da271f95a385fb63b
Jhonantan Ernane <jhonsoad@live.com>
2019-04-07 21:15:48 -0300

    criação das paginas de login e landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'landing', loadChildren: './landing/landing.module#LandingPageModule' },
+  { path: 'login', loadChildren: './landing/login/login.module#LoginPageModule' },
 ];
 
 @NgModule({
